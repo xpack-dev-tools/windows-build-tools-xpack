93537f2e96cb1a59cd133861933a56bc45e00003
Liviu Ionescu <ilg@livius.net>
2024-09-18 14:09:06 +0300

    website: updates

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -61,6 +61,13 @@ function getCustomFields() {
     upstreamVersion = xpackSemver;
   }
 
+  const websiteFilePath = path.join(path.dirname(path.dirname(pwd)), 'website', 'package.json');
+  // logger.info(filePath);
+  const websiteFileContent = fs.readFileSync(websiteFilePath);
+  const websitePackageJson = JSON.parse(websiteFileContent.toString());
+
+  const docusaurusVersion = websitePackageJson.dependencies['@docusaurus/core'].replace ('^', '');
+
   return {
     appName: rootPackageJson.xpack.properties.appName,
     appLcName: rootPackageJson.xpack.properties.appLcName,
@@ -70,6 +77,7 @@ function getCustomFields() {
     xpackSubversion,
     npmSubversion,
     upstreamVersion,
+    docusaurusVersion,
     ...customFields,
   }
 }
