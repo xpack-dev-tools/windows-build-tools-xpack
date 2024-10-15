21e81ca125bd3b3d586e1ac6f5b2110ed08bea25
Liviu Ionescu <ilg@livius.net>
2024-10-15 03:34:27 +0300

    website: updates

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -42,7 +42,7 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout 
-      title={`Welcome to windows-build-tools`}
+      title="Welcome!"
       description="A binary package with the windows-build-tools executables">
       <HomepageHeader />
       <main>
