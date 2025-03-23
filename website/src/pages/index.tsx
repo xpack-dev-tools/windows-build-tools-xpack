e8c6203c88276091bae00c1f2b429a00f8d0acbd
Liviu Ionescu <ilg@livius.net>
2025-03-23 20:01:43 +0200

    website: re-generate commons

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -9,7 +9,7 @@
  * for any purpose is hereby granted, under the terms of the MIT license.
  *
  * If a copy of the license was not distributed with this file, it can
- * be obtained from https://opensource.org/licenses/MIT/.
+ * be obtained from https://opensource.org/licenses/MIT.
  */
 
 import React from 'react';
@@ -36,7 +36,9 @@ function HomepageHeader() {
         </span>
         </p>
         <div className={styles.installWithCopy}>
-          <InstallWithCopy>xpm install @xpack-dev-tools/windows-build-tools@4.4.1-3.1 --verbose</InstallWithCopy>
+          <InstallWithCopy>xpm install @xpack-dev-tools/windows-build-tools@4.4.1-3.1 --verbose
+          </InstallWithCopy>
+
         </div>
       </div>
     </header>
